70b244c45c8542e80157a566bd59c8bb71c5bad0
avpork <avporsk@gmail.com>
2021-05-24 00:04:20 +0500

    New BotLib

---
+++ Quik Telegram Messenger.lua
@@ -0,0 +1,9 @@
+-- Отправка состояния и счета в телеграм
+-- подключаемые модули
+
+package.cpath = getScriptPath().."\\TelegramBot\\BotLib.dll"
+require("BotLib");  
+
+
+
+
--- QuikTelegramMessenger.lua
+++
@@ -1,9 +0,0 @@
--- Отправка состояния и счета в телеграм
--- подключаемые модули
-
-package.cpath = getScriptPath().."\\TelegramBot\\TelegramBotC.dll"
-require("Telegram");  
-
-
-
-
